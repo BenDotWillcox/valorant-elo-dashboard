58ece3e5f1c1ce2d1bb8737bc15ff406b1455c6a
BenDotWillcox <benjamin.willcox2@gmail.com>
2025-01-12 08:30:08 -0600

    adding shadcn components

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
 export default function Home() {
   return (
-    <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-      <h1>Hello World</h1>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>Home Page</div>
     </main>
   );
 }
-

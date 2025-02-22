a3bfba4dcd2e8fb2d8969e4b6e5b9efd883becec
BenDotWillcox <benjamin.willcox2@gmail.com>
2025-02-22 08:37:00 -0600

    add everything to schema

--- db/schema.ts
+++ db/schema.ts
@@ -1,4 +1,4 @@
-import { pgTable, varchar, decimal, timestamp, integer } from "drizzle-orm/pg-core";
+import { pgTable, varchar, decimal, timestamp, integer, boolean } from "drizzle-orm/pg-core";
 
 // Types
 export type NewEloRatingCurrent = {
@@ -10,6 +10,21 @@ export type NewEloRatingCurrent = {
 };
 
 // Tables
+export const seasonsTable = pgTable('seasons', {
+  id: integer('id').primaryKey(),
+  year: integer('year').notNull(),
+  isActive: boolean('is_active').notNull().default(false),
+});
+
+export const mapsTable = pgTable('maps', {
+  id: integer('id').primaryKey(),
+  mapName: varchar('map_name').notNull(),
+  completedAt: timestamp('completed_at').notNull(),
+  winnerScore: integer('winner_score').notNull(),
+  loserScore: integer('loser_score').notNull(),
+  seasonId: integer('season_id').notNull(),
+});
+
 export const elo_ratings_current = pgTable('elo_ratings_current', {
   teamId: integer('team_id').notNull(),
   mapName: varchar('map_name').notNull(),
